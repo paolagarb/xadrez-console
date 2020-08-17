059c7008e523c7c3771c269de2f1ba60e9cb5366
Paola Garbato <paolagarbato@gmail.com>
2020-08-17 14:37:22 -0300

    Adicionando métodos ao Tabuleiro e criando Exceções

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 using System.Xml;
 using tabuleiro;
 
@@ -9,12 +10,21 @@ namespace Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
-            
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-            Tela.ImprimirTabuleiro(tab);
+            try
+            {
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+           
 
         }
 
