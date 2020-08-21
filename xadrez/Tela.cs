7ba3d76ae537555e2d7a8cb7044b5b0957236c17
Paola Garbato <paolagarbato@gmail.com>
2020-08-21 16:30:52 -0300

    Corrigindo tabuleiro desalinhado

--- xadrez/Tela.cs
+++ xadrez/Tela.cs
@@ -8,38 +8,41 @@ namespace Xadrez
     {
         public static void ImprimirTabuleiro(Tabuleiro tab)
         {
-            for (int i=0; i<tab.Linhas; i++)
+            for (int i = 0; i < tab.Linhas; i++)
             {
-                Console.Write(8-i + "    ");
-                for (int j=0; j<tab.Colunas; j++)
+                Console.Write(8 - i + "    ");
+                for (int j = 0; j < tab.Colunas; j++)
                 {
-                    if (tab.Peca(i, j) == null)
-                    {
-                        Console.Write("- ");
-                    }
-                    else
-                    {
-                        ImprimirPeca(tab.Peca(i, j));
-                        Console.Write(" ");
-                    }
+                    ImprimirPeca(tab.Peca(i, j));
+                    Console.Write(" ");
                 }
                 Console.WriteLine();
             }
             Console.WriteLine();
-            Console.WriteLine("     a b c d e f g h ");
+            Console.WriteLine("     a  b  c  d  e  f  g  h ");
         }
 
         public static void ImprimirPeca(Peca peca)
         {
-            if (peca.Cor == Cor.Branca)
+
+            if (peca == null)
             {
-                Console.Write(peca);
-            } else
+                Console.Write("- ");
+            }
+            else
             {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+                if (peca.Cor == Cor.Branca)
+                {
+                    Console.Write(peca + " ");
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(peca + " ");
+                    Console.ForegroundColor = aux;
+                }
+                //Console.WriteLine(" ");
             }
         }
 
@@ -47,7 +50,7 @@ namespace Xadrez
         {
             string s = Console.ReadLine();
             char coluna = s[0];
-            int linha = int.Parse(s[1] + "");
+            int linha = int.Parse(s[1] + " ");
             return new PosicaoXadrez(coluna, linha);
         }
     }
